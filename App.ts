c924074d484934695cbb75f710b7b10461b96ef7
Alejandro Sobko <alejandro.sobko@gmail.com>
2021-12-19 00:53:20 -0300

    Fix cors

--- App.ts
+++ App.ts
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 import { Routes } from './routes/location.routes';
+
+const CORS_OPTIONS = {
+    origin: '*',
+    methods: [
+        'POST',
+    ],
+    allowedHeaders: [
+        'Content-Type',
+    ],
+};
 
 export class App {
 
@@ -21,6 +32,7 @@ export class App {
         this.express.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
         this.express.use(bodyParser.json({ type: 'application/vnd.api+json' }));
         this.express.use(bodyParser.urlencoded({ extended: false }));
+        this.express.use(cors(CORS_OPTIONS));
     }
 
     // Init Routes.
